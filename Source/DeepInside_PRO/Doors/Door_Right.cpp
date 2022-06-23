fd9a1bc2284710adc3b4f4e530c4acf854403cd4
Mauricio Rodriguez Becerra <mrodriguezbec@unal.edu.co>
2022-06-23 13:33:31 -0500

    Project Update

---
+++ Source/DeepInside_PRO/Doors/Door_Right.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Door_Right.h"
+
+
+// Sets default values
+ADoor_Right::ADoor_Right()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ADoor_Right::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADoor_Right::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/DeepInside_PRO/Doors/Door_Right.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Door_Right.generated.h"
+
+UCLASS()
+class DEEPINSIDE_PRO_API ADoor_Right : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ADoor_Right();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
